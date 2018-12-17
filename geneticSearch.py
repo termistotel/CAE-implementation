5de1e74162a8e02233af6c7cf924651e422ab82b
termistotel <termistotel@gmail.com>
2018-12-17 02:42:17 +0100

    Update gitignore, few bugfixes

--- geneticSearch.py
+++ geneticSearch.py
@@ -70,10 +70,9 @@ def randomInit():
 	hparameters["convPerLayer"] = int(random.randint(1, 4))
 	hparameters["deconvPerLayer"] = int(random.randint(1, 3))
 	hparameters["dLayNum"] = int(random.gauss(1, 0.4))
-	hparameters["dLayNum"] = 0
-
 	if hparameters["dLayNum"] < 0:
 		hparameters["dLayNum"] = 1
+	hparameters["dLayNum"] = 0
 
 	hparameters["dLayNeurBase"] = random.gauss(0.5, 0.1)
 	if hparameters["dLayNeurBase"] < 0.01:
@@ -117,7 +116,8 @@ def generateNewGene(name, **kwargs):
 		return 1 - 0.1**random.gauss(3, 0.4)
 
 	if name == "f":
-		return int(random.gauss(3.5, 0.4))
+		# return int(random.gauss(3.5, 0.4))
+		return 3
 
 	if name == "convLayerNum":
 		cLN = int(random.gauss(2.5, 0.4))
@@ -132,7 +132,8 @@ def generateNewGene(name, **kwargs):
 
 	if name == "dLayNum":
 		dLayNum = int(random.gauss(1,0.4))
-		return clip(dLayNum, 1)
+		# return clip(dLayNum, 1)
+		return 0
 
 	if name == "dLayNeurBase":
 		return clip(random.gauss(0.5, 0.1), 0.01)
@@ -173,22 +174,20 @@ if __name__ == '__main__':
 			# fitness.append(1/np.sum(np.square(tmp-test)))
 			model = CAE(imgs.shape[1:], hparameter)
 			testLoss, trainLoss = model.train(train, test, trainMasks, testMasks,
-											dirname=os.path.join(os.path.join("summaries", str(generation)), str(i)),
+											dirname=os.path.join(os.path.join("summaries", str(generation)), str(generation)+'_'+str(i)),
 											niter=200,
 											batchsize=1,
 											display=False,
 											printLoss=False)
 			fitness.append((testLoss+trainLoss)/2.0)
 
+		# Report
+		chosen = list(np.argsort(fitness))[:5]
+		with open(os.path.join(os.path.join("summaries", str(generation)), "geneticReport"), "w") as f:
+			f.write(str(chosen)+"\n"+str(list(sorted(fitness))) + "\n" + "Best of generation " + str(generation) + str(hparameters[chosen[0]]))
 
 		# Selection
 		newHparameters = []
-		chosen = list(reversed(np.argsort(fitness)))[:5]
-		tmp = np.array(list(map(lambda x: x[1], hparameters[chosen[0]].items())))
-		print("Best of generation " + str(generation), tmp)
-		with open(os.path.join(os.path.join("summaries", str(generation)), "geneticReport"), "w") as f:
-			f.write(str(chosen)+"\n"+str(list(reversed(sorted(fitness)))))
-
 		for i, index1 in enumerate(chosen):
 			for index2 in chosen[i:]:
 				genes1 = hparameters[index1]
@@ -199,7 +198,7 @@ if __name__ == '__main__':
 		for child in newHparameters:
 			for key in child:
 				r = random.random()
-				if r < 0.4:
+				if r < 0.1:
 					child[key] = generateNewGene(key)
 
 		# Add random children
