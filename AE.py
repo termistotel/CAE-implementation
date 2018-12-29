fce1d22d400d3df2b94b85e2102cf66d6125cde1
termistotel <termistotel@gmail.com>
2018-12-29 23:17:46 +0100

    Implement database loading of train data

--- AE.py
+++ AE.py
@@ -12,7 +12,7 @@ hparametersDef = {"alphaTau": 10000, "alpha0": 0.001, "lam": 0.001, "betaMom": 0
 				"dLayNum": 0, "dLayNeurBase": 0.1, "latDim": 20}
 
 class CAE():
-	def __init__(self, dataShape, hparameters=hparametersDef):
+	def __init__(self, trainDB, testDB, dataShape, hparameters=hparametersDef):
 		self.alphaTau = hparameters["alphaTau"]
 		self.alpha0 = hparameters["alpha0"]
 		self.lam = hparameters["lam"]
@@ -36,90 +36,95 @@ class CAE():
 
 		self.latDim = hparameters["latDim"]
 
-		graph = self.createGraph(dataShape)
+		self.createGraph(trainDB, testDB, dataShape)
 
-	def createGraph(self, dataShape):
-		self.graph = tf.Graph()
+	def createGraph(self, trainDB, testDB, dataShape):
+		# self.graph = tf.Graph()
 		self.shapes = []
-		with self.graph.as_default():
-			x = tf.placeholder(shape= ([None]+list(dataShape)), dtype=tf.float32, name="X")
-			self.x = x
-			self.lr = tf.placeholder(tf.float32, shape=(), name="learning_rate")
-			gs = tf.Variable(0, trainable=False)
+		# x = tf.placeholder(shape= ([None]+list(dataShape)), dtype=tf.float32, name="X")
+		# x = tf.constant(trainDB, dtype=tf.float32, name="x")
+		iter = tf.data.Iterator.from_structure(trainDB.output_types, trainDB.output_shapes)
+		self.train_init = iter.make_initializer(trainDB)
+		# self.test_init = iter.make_initializer(testDB)
 
-			# create convolutional layers
-			A = x
-			self.shapes.append(tf.shape(A))
-			convLayersOut = []
-			for i in range(self.convLayerNum):
-				with tf.variable_scope("Conv"+str(i)):
-					for j in range(self.convPerLayer):
-						A = tf.layers.conv2d(A, self.filterNum[i], (self.f, self.f), (1,1), padding="SAME", activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=self.lam))
-						A = tf.layers.dropout(A)
-					if i < (self.convLayerNum-1):
-						A = tf.layers.max_pooling2d(A, (2,2), (2,2), padding="SAME")
-					self.shapes.append(tf.shape(A))
-					convLayersOut.append(A)
+		x = tf.to_float(iter.get_next())
+		self.x = x
+		self.lr = tf.placeholder(tf.float32, shape=(), name="learning_rate")
+		gs = tf.Variable(0, trainable=False)
 
-			# create dense layers
-			A = self.createDenseLayers(A)
+		# create convolutional layers
+		A = x
+		self.shapes.append(tf.shape(A))
+		convLayersOut = []
+		for i in range(self.convLayerNum):
+			with tf.variable_scope("Conv"+str(i)):
+				for j in range(self.convPerLayer):
+					A = tf.layers.conv2d(A, self.filterNum[i], (self.f, self.f), (1,1), padding="SAME", activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=self.lam))
+					A = tf.layers.dropout(A)
+				if i < (self.convLayerNum-1):
+					A = tf.layers.max_pooling2d(A, (2,2), (2,2), padding="SAME")
+				self.shapes.append(tf.shape(A))
+				convLayersOut.append(A)
 
-			# create upscale layers
-			convLayersOut.pop()
-			for i in list(reversed(range(self.convLayerNum)))[1:]:
-				with tf.variable_scope("Upscale"+str(i+1)):
-					A = tf.image.resize_images(A, size=tf.shape(convLayersOut.pop())[1:3])
-					for j in range(self.deconvPerLayer):
-						A = tf.layers.conv2d(A, self.filterNum[i], (self.f, self.f), (1,1), padding="SAME", activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=self.lam))
-						A = tf.layers.dropout(A)
-					self.shapes.append(tf.shape(A))
+		# create dense layers
+		A = self.createDenseLayers(A)
 
-			# Output layer
-			with tf.variable_scope("Upscale"+str(0)):
-				A = tf.image.resize_images(A, size=tf.shape(x)[1:3])
+		# create upscale layers
+		convLayersOut.pop()
+		for i in list(reversed(range(self.convLayerNum)))[1:]:
+			with tf.variable_scope("Upscale"+str(i+1)):
+				A = tf.image.resize_images(A, size=tf.shape(convLayersOut.pop())[1:3])
 				for j in range(self.deconvPerLayer):
-					A = tf.layers.conv2d(A, 3, (self.f, self.f), (1,1), padding="SAME", activation=tf.nn.sigmoid, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=self.lam))
+					A = tf.layers.conv2d(A, self.filterNum[i], (self.f, self.f), (1,1), padding="SAME", activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=self.lam))
 					A = tf.layers.dropout(A)
 				self.shapes.append(tf.shape(A))
-				self.out = A
 
-			self.loss = tf.losses.mean_squared_error(x, self.out) + tf.losses.get_regularization_loss()
+		# Output layer
+		with tf.variable_scope("Upscale"+str(0)):
+			A = tf.image.resize_images(A, size=tf.shape(x)[1:3])
+			for j in range(self.deconvPerLayer):
+				A = tf.layers.conv2d(A, 3, (self.f, self.f), (1,1), padding="SAME", activation=tf.nn.sigmoid, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=self.lam))
+				A = tf.layers.dropout(A)
+			self.shapes.append(tf.shape(A))
+			self.out = A
 
-			self.learning_rate = tf.train.exponential_decay(self.lr, gs, self.alphaTau, 0.1)
-			optimizer = tf.train.AdamOptimizer(learning_rate = self.learning_rate)
-			self.optimize = optimizer.minimize(self.loss, global_step = gs)
+		self.loss = tf.losses.mean_squared_error(x, self.out) + tf.losses.get_regularization_loss()
 
-			grads = optimizer.compute_gradients(self.loss)
+		self.learning_rate = tf.train.exponential_decay(self.lr, gs, self.alphaTau, 0.1)
+		optimizer = tf.train.AdamOptimizer(learning_rate = self.learning_rate)
+		self.optimize = optimizer.minimize(self.loss, global_step = gs)
 
-			gradSummaries = []
-			for g,v in grads:
-				if not g is None:
-					with tf.name_scope("grads"):
-						gnorm = tf.sqrt(tf.reduce_mean(tf.square(g)))
-						gradSummaries.append(tf.summary.scalar(v.name, gnorm))
-				else:
-					print("Vanishing Grad: ", v.name, g)
-			gSummaries = tf.summary.merge(gradSummaries)
+		grads = optimizer.compute_gradients(self.loss)
 
-			with tf.name_scope("loss_data"):
-				self.testLoss = tf.placeholder(tf.float32, shape=None, name="test_loss_summary")
-				self.trainLoss = tf.placeholder(tf.float32, shape=None, name="train_loss_summary")
-				testLossSummary = tf.summary.scalar("TestLoss", self.testLoss)
-				trainLossSummary = tf.summary.scalar("TrainLoss", self.trainLoss)
-				logTestLossSummary = tf.summary.scalar("TestLossLog", tf.log(self.testLoss))
-				logTrainLossSummary = tf.summary.scalar("TrainLossLog", tf.log(self.trainLoss))
-			lossSummaries = tf.summary.merge([testLossSummary, logTestLossSummary, trainLossSummary, logTrainLossSummary])
+		gradSummaries = []
+		for g,v in grads:
+			if not g is None:
+				with tf.name_scope("grads"):
+					gnorm = tf.sqrt(tf.reduce_mean(tf.square(g)))
+					gradSummaries.append(tf.summary.scalar(v.name, gnorm))
+			else:
+				print("Vanishing Grad: ", v.name, g)
+		gSummaries = tf.summary.merge(gradSummaries)
 
-			self.summaries=tf.summary.merge([gSummaries, lossSummaries])
-			self.saver = tf.train.Saver()
+		with tf.name_scope("loss_data"):
+			self.testLoss = tf.placeholder(tf.float32, shape=None, name="test_loss_summary")
+			self.trainLoss = tf.placeholder(tf.float32, shape=None, name="train_loss_summary")
+			testLossSummary = tf.summary.scalar("TestLoss", self.testLoss)
+			trainLossSummary = tf.summary.scalar("TrainLoss", self.trainLoss)
+			logTestLossSummary = tf.summary.scalar("TestLossLog", tf.log(self.testLoss))
+			logTrainLossSummary = tf.summary.scalar("TrainLossLog", tf.log(self.trainLoss))
+		lossSummaries = tf.summary.merge([testLossSummary, logTestLossSummary, trainLossSummary, logTrainLossSummary])
 
-		return self.graph
+		self.summaries=tf.summary.merge([gSummaries, lossSummaries])
+		self.saver = tf.train.Saver()
+
+		# return self.graph
 
 	def createDenseLayers(self,x):
 		N=np.prod(x.get_shape().as_list()[1:])
 		with tf.variable_scope("Dense"):
 			A = tf.layers.flatten(x)
-			A = tf.layers.dense(A, self.latdim, activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=self.lam))
+			A = tf.layers.dense(A, self.latDim, activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=self.lam))
 			self.feat = A
 			A = tf.layers.dropout(A)
 			self.shapes.append(tf.shape(A))
@@ -132,7 +137,7 @@ class CAE():
 			self.shapes.append(tf.shape(A))
 		return A
 
-	def train(self, train, test, dirname="summaries",  niter=1000, batchsize=2, display=False, restart=True, printLoss=True):
+	def train(self, batchnum, dirname="summaries",  niter=1000, batchsize=2, display=False, restart=True, printLoss=True):
 		config = tf.ConfigProto(log_device_placement=True)
 		# config.gpu_options.allow_growth = True
 		hparameters = {
@@ -158,46 +163,44 @@ class CAE():
 		with open(os.path.join(dirname,"hparameters"), "w") as f:
 			f.write(json.dumps(hparameters))
 
-		summ_writer = tf.summary.FileWriter(dirname, self.graph)
+		summ_writer = tf.summary.FileWriter(dirname)
 
-		with tf.Session(graph=self.graph, config=config) as sess:
+		with tf.Session(config=config) as sess:
 
 			if restart:
 				sess.run(tf.global_variables_initializer())
 
+				sess.run(self.train_init)
+
 			if display:
 				print("Architecture:")
-				for shape in sess.run(self.shapes, feed_dict={self.x:train}):
+				for shape in sess.run(self.shapes):
 					print(shape)
 				input("Press any key to continue...")
-
-			M = train.shape[0]
 
 			for epoch in range(niter):
 				print(epoch)
 
-				# Shuffle train data
-				shuffleList = np.arange(M)
-				np.random.shuffle(shuffleList)
 
-				train = train[shuffleList]
+				finTrain = 0
+				for j in range(batchnum):
+					_, tmp = sess.run([self.optimize, self.loss], feed_dict={self.lr: self.alpha0})
+					finTrain+=tmp
+				finTrain/=batchnum
 
-				for j in range(M//batchsize):
-					sess.run(self.optimize, feed_dict={self.x: train[j*batchsize:(j+1)*batchsize], self.lr: self.alpha0})
+				# summ = sess.run(self.summaries, feed_dict={
+				# 						self.trainLoss: sess.run(self.loss, feed_dict={self.x:train}),
+				# 						self.testLoss: sess.run(self.loss, feed_dict={self.x:test}),
+				# 						self.x: train[j*batchsize:(j+1)*batchsize]
+				# 						# self.mask: trainMasks[j*batchsize:(j+1)*batchsize]
+				# 						})
+				# summ_writer.add_summary(summ, epoch)
 
-				summ = sess.run(self.summaries, feed_dict={
-										self.trainLoss: sess.run(self.loss, feed_dict={self.x:train}),
-										self.testLoss: sess.run(self.loss, feed_dict={self.x:test}),
-										self.x: train[j*batchsize:(j+1)*batchsize]
-										# self.mask: trainMasks[j*batchsize:(j+1)*batchsize]
-										})
+				# finTest = sess.run(self.loss, feed_dict={self.x:test})
+				# finTrain = sess.run(self.loss, feed_dict={self.x:train})
 
-				summ_writer.add_summary(summ, epoch)
-
-				finTest = sess.run(self.loss, feed_dict={self.x:test})
-				finTrain = sess.run(self.loss, feed_dict={self.x:train})
 				if printLoss:
-					print("Test: ", finTest)
+					# print("Test: ", finTest)
 					print("Train: ", finTrain)
 
 			with open(os.path.join(dirname,"result"), "w") as f:
@@ -206,26 +209,23 @@ class CAE():
 			self.saver.save(sess, os.path.join(dirname,"model"))
 
 			if display:
-				out = sess.run(self.out, feed_dict={self.x: test})
-				out2 = sess.run(self.out, feed_dict={self.x: out})
-				for i, val in enumerate(out2):
-					plt.imshow(test[i])
-					plt.show()
-					plt.imshow(out[i])
-					plt.show()
-					plt.imshow(val)
-					plt.show()
+				# out = sess.run(self.out, feed_dict={self.x: test})
+				# out2 = sess.run(self.out, feed_dict={self.x: out})
+				# for i, val in enumerate(out2):
+				# 	plt.imshow(test[i])
+				# 	plt.show()
+				# 	plt.imshow(out[i])
+				# 	plt.show()
+				# 	plt.imshow(val)
+				# 	plt.show()
 
 				print("test done")
 
-				out = sess.run(self.out, feed_dict={self.x: train})
-				out2 = sess.run(self.out, feed_dict={self.x: out})
-				for i, val in enumerate(out2):
+				out = sess.run(self.out)
+				for i, val in enumerate(out):
 					plt.imshow(train[i])
 					plt.show()
 					plt.imshow(out[i])
-					plt.show()
-					plt.imshow(val)
 					plt.show()
 
 		return finTest, finTrain
--- main.py
+++ main.py
@@ -6,13 +6,11 @@ import cv2
 import random
 import json
 from AE import CAE, hparametersDef
+import argparse
+from PIL import Image
 
-niter = 1000
-batchsize = 1
-sumdir = "summaries"
-datadir = "data"
+import tensorflow as tf
 
-resize = 0.04
 
 def loadHparams(dir):	
 	with open(os.path.join(dir,"hparameters"), "r") as f:
@@ -36,19 +34,85 @@ def loadHparams(dir):
 
 	return hparams
 
-# if __name__ == '__main__':
-imgs = np.array([cv2.cvtColor(cv2.resize(cv2.imread("data/"+file), (0,0), fx=resize, fy=resize), cv2.COLOR_BGR2RGB) for file in sorted(os.listdir("data"))])/255
+p = argparse.ArgumentParser()
 
-i = 0
-# random.seed(1337)
+p.add_argument("--imgs", required=True, type=str, default=None, help='input image dir')
+p.add_argument("--sums", required=False, type=str, default="summaries", help='summaries dir')
+p.add_argument("--rescale", required=False, type=int, default=1, help='resize scale')
+p.add_argument("--niter", required=False, type=int, default=1000, help='number of iterations')
+p.add_argument("--batch-size", required=False, type=int, default=1, help='batch size')
+p.add_argument("--in-memory", action='store_true', help='Keep data in memory')
+p.add_argument("--remake", action='store_true', help='Remake database')
 
-shuffleList = np.arange(len(imgs))
-np.random.shuffle(shuffleList)
+args = p.parse_args()
 
-imgs = imgs[shuffleList]
+datadir = args.imgs
+niter = args.niter
+batchsize = args.batch_size
+sumdir = args.sums
+rescale = args.rescale
+inMem = args.in_memory
+remake = args.remake
 
-train = imgs[:-20]
-test = imgs[-20:]
+minShape = sorted(map(lambda file: Image.open(os.path.join(datadir, file)).size, sorted(os.listdir(datadir))))[0]
+imgs = None
+
+# Remake databases
+if remake:
+	print("Rebuiling databases...")
+	imgs = np.array([cv2.cvtColor(cv2.resize(cv2.imread(os.path.join(datadir, file)), minShape), cv2.COLOR_BGR2RGB) for file in sorted(os.listdir(datadir))])/255
+	# random.seed(1337)
+	shuffleList = np.arange(len(imgs))
+	np.random.shuffle(shuffleList)
+	imgs = imgs[shuffleList]
+
+	train = imgs[:-20]
+	test = imgs[-20:]
+
+	with open("train.data", 'wb') as data:
+		for img in train:
+			data.write((img).reshape(-1).tostring())
+
+	with open("test.data", 'wb') as data:
+		for img in test:
+			data.write((img).reshape(-1).tostring())
+# Load one image for reference
+else:
+	file = os.listdir(datadir)[0]
+	img = cv2.cvtColor(cv2.resize(cv2.imread(os.path.join(datadir, file)), minShape), cv2.COLOR_BGR2RGB)/255
+
+# Setting up database
+if inMem:
+	print("Setting up database from memory...")
+	if imgs is None:
+		imgs = np.array([cv2.cvtColor(cv2.resize(cv2.imread(os.path.join(datadir, file)), minShape), cv2.COLOR_BGR2RGB) for file in sorted(os.listdir(datadir))])
+		# random.seed(1337)
+		shuffleList = np.arange(len(imgs))
+		np.random.shuffle(shuffleList)
+		imgs = imgs[shuffleList]
+
+		train = imgs[:-20]
+		test = imgs[-20:]
+
+	trainDB = tf.data.Dataset.from_tensor_slices(train)
+	testDB = tf.data.Dataset.from_tensor_slices(test)
+
+else:
+	print("Setting up database from files: " + "train.data" + ", " + "test.data")
+	trainDB = tf.data.FixedLengthRecordDataset("train.data", np.prod(img.shape)*img.dtype.itemsize)
+	testDB = tf.data.FixedLengthRecordDataset("test.data", np.prod(img.shape)*img.dtype.itemsize)
+
+	trainDB = trainDB.map(lambda x: tf.reshape(tf.decode_raw(x, img.dtype), img.shape))
+	testDB = testDB.map(lambda x: tf.reshape(tf.decode_raw(x, img.dtype), img.shape))
+
+trainDB = trainDB.shuffle(100).repeat().batch(batchsize)
+testDB = testDB.shuffle(100).repeat().batch(1)
+
+batchnum = int(len(os.listdir(datadir))/batchsize)
+
+test, train, imgs = None, None, None
+
+
 
 hparameters = {}
 
@@ -67,16 +131,16 @@ hparameters = loadHparams(os.path.join(sumdir, str(21), "21_0"))
 # hparameters["dLayNeurBase"] = 0.5
 # hparameters["filterNum0"] = 8
 # hparameters["filterBase"] = 1.8365
+hparameters["latDim"] = 20
 
 hparameters["batchsize"] = batchsize
 hparameters["niter"] = niter
 
-print(train.shape)
 print(hparameters)
 input("Press any key to continue")
 
-model = CAE(imgs.shape[1:], hparameters)
-model.train(train, test,
+model = CAE(trainDB, testDB, img.shape, hparameters)
+model.train(batchnum,
 			dirname=os.path.join(sumdir, "test"),
 			niter=hparameters["niter"],
 			batchsize=hparameters["batchsize"],
@@ -84,3 +148,4 @@ model.train(train, test,
 			printLoss=True)
 
 i+=1
+
